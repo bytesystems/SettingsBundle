660d015625b232d7af3d577d3431ffca17b6ca83
Daniel Heim <heim@bytesystems.de>
2022-04-20 22:26:48 +0200

    - remove twig extension - add symfony 6

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,7 +12,7 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('bytesystes_settings');
+        $treeBuilder = new TreeBuilder('bytesystems_settings');
         return $treeBuilder;
     }
 }
